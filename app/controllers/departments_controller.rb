3cc696d487fbf7670ccd3f76bbcb329a7a392d6c
sjohnston92 <simonjohnston.c@gmail.com>
2020-08-26 16:55:03 -0600

    Controllers and Modules all going for departments

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,32 +1,49 @@
 class DepartmentsController < ApplicationController
 
-  # before_action :set_department, only: [ :show, :edit , :update, :destroy]
+  before_action :set_d, only: [:show, :edit , :update, :destroy]
   
   def index
-    @departments = Department 
+    @departments = Department.all
   end
 
   def show
-    @department = Department.find(params[:id])
-    
+    # @department = Department.find(params[:id])
   end
 
   def new
     @department = Department.new
+    render partial:"form"
   end
 
   def edit
+    render partial:"form"
+
   end
 
   def create
+    @department = Department.new(d_params)
+
+    if @department.save
+      redirect_to  departments_path
+
+    else
+      render :new
+    
+    end
   end
 
   def destroy
+    set_d.destroy
+    redirect_to  departments_path
   end
 
   private
 
-  def set_department
+   def d_params
+    params.require(:department).permit(:name)
+    end
+
+  def set_d
     @department = Department.find(params[:id])
   end
 
